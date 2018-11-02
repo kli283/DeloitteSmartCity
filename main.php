d6bd9bfa0d259bba4768f675f71baa39e77b4ff2
Hyunrae Kim <hyunrae.kim@gmail.com>
2018-11-03 05:25:38 +0800

    unnecessary category

--- main.php
+++ main.php
@@ -48,7 +48,6 @@ if (!empty($_GET['id'])) {
               $stmt->close();
     }
 
-    $category = $_GET['category'];
     $stmt = $mysqli->prepare("SELECT name, latitude, longitude, category FROM locations WHERE id=?");
     if (!$stmt)
     {
