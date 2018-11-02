36b3fa1db36c3369f36c0dfc48dcc45aa1f5e8a4
Hyunrae Kim <hyunrae.kim@gmail.com>
2018-11-02 22:22:19 +0800

    testing search

--- main.php
+++ main.php
@@ -28,87 +28,67 @@ if (empty($_GET['id'])) {
 ?>
 
 
-<!doctype html>
-  <html>
+<!DOCTYPE html>
+<html>
     <head>
-      <meta charset="UTF-8">
-      <title> <?php echo $name ?>  </title>
-      <style>
-      /* Set the size of the div element that contains the map */
-      #map {
-        height: 400px;  /* The height is 400 pixels */
-        width: 100%;  /* The width is the width of the web page */
-       }
-       #map2 {
-         height: 400px;  /* The height is 400 pixels */
-         width: 100%;  /* The width is the width of the web page */
-        }
-    </style>
+        <title> <?php echo $name ?> </title>
+        <style>
+            /* Set the size of the div element that contains the map */
+            #map
+            {
+                height: 400px;  /* The height is 400 pixels */
+                width: 100%;  /* The width is the width of the web page */
+            }
+        </style>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
 
     <body>
-          <h3>TekChange</h3>
-          <h3>Name: &nbsp <?php echo $name ?> </h3>
-          <h3>ID: &nbsp <?php echo $chekpoint_id?> </h3>
+        <div class="topnav">
+          <form action="main.php" method="get">
+              Search: <input name="chekpoint_id" type="text" placeholder="Type in ID"> &nbsp
+              <input class="button" name="submit" type="submit" value="search">
+          </form>
 
-        <!--The div element for the map -->
+        </div>
+        <h1>Name: <?php echo $name ?> </h1>
+        <h1>ID: <?php echo $chekpoint_id?> </h1>
         <div id="map"></div>
-        <b>Mode of Travel: </b>
-      <select id="mode">
-        <option value="WALKING">Walking</option>
-        <option value="DRIVING">Driving</option>
-      </select>
+        <div class="bottom">
+            <button>Show in Map</button>
+            <select id="categories">
+                <option value="defaultSelect">- SELECT -</option>
+                <option value="food">Food</option>
+                <option value="shopping">Shopping</option>
+                <option value="bank">Banks</option>
+                <option value="mtr">MTR</option>
+            </select>
 
-      <div id="map2"></div>
+            <div class="listing">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+
         <script>
-    // Initialize and add the map
-    function initMap() {
-      // The location of Uluru
-      var tempMap = {lat: 22.2780691, lng: 114.16490905};
-      // The map, centered at Uluru
-      var map = new google.maps.Map(
-          document.getElementById('map'), {zoom: 20, center: tempMap,disableDefaultUI: true});
-      // The marker, positioned at Uluru
-      var marker = new google.maps.Marker({position: tempMap, map: map});
-
-      var directionsDisplay = new google.maps.DirectionsRenderer;
-      var directionsService = new google.maps.DirectionsService;
-      var map2 = new google.maps.Map(document.getElementById('map2'), {
-        zoom: 14,
-        center: {lat: 37.77, lng: -122.447},
-        disableDefaultUI: true
-      });
-      directionsDisplay.setMap(map2);
-
-      calculateAndDisplayRoute(directionsService, directionsDisplay);
-      document.getElementById('mode').addEventListener('change', function() {
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
-      });
-    }
-
-    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-      var selectedMode = document.getElementById('mode').value;
-      directionsService.route({
-        origin: {lat: 22.2780691, lng: 114.16490905},  // Haight.
-        destination: {lat: 22.27851332, lng: 114.16481472},  // Ocean Beach.
-        // Note that Javascript allows us to access the constant
-        // using square brackets and a string value as its
-        // "property."
-        travelMode: google.maps.TravelMode[selectedMode]
-      }, function(response, status) {
-        if (status == 'OK') {
-          directionsDisplay.setDirections(response);
-        } else {
-          window.alert('Directions request failed due to ' + status);
-        }
-      });
-    }
+            function initMap()
+            {
+                var map = new google.maps.Map(document.getElementById('map'),
+                {
+                  zoom: 4,
+                  center: {lat: -33, lng: 151},
+                  disableDefaultUI: true
+                });
+            }
         </script>
-
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBR8YK6VN7gr_t3DH6ywMiC0sejSzb3Lyc&callback=initMap">
-    </script>
-
-
-    </body>
-  </html>
+        <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC67ZO8RUoSPyKlHm3gF7iAbPKdE00C5sM&callback=initMap">
+        </script>
+  </body>
+</html>
