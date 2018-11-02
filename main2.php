1f899226d13edd41f5883c1f8a8094cb546e04d0
Hyunrae Kim <hyunrae.kim@gmail.com>
2018-11-03 05:06:49 +0800

    change form

--- main2.php
+++ main2.php
@@ -94,15 +94,16 @@ if (empty($_GET['id'])) {
         <div id="map"></div>
         <div class="bottom">
             <button><?php echo '<a href="http://maps.google.com/maps?q=' .$latitude. ',' .$longitude. '"> </a>' ?> Show in Map</button>
-              <form action="#" method="get">
-                  <select id="categories">
-                      <option value="defaultSelect">- SELECT -</option>
-                      <option value="food">Food</option>
-                      <option value="shopping">Shopping</option>
-                      <option value="bank">Banks</option>
-                      <option value="mtr">MTR</option>
-                  </select>
-                  <button type="submit" name="id" value="<?php echo $id?>">Submit</button><br> <br>
+            <form action="main.php" method="get" id="categories">
+                    <input type="hidden" />
+                    Category: <select id="categories" name='category' >
+                        <option selected disabled>Choose here</option>
+                        <option value="food">Food</option>
+                        <option value="shopping">Shopping</option>
+                        <option value="bank">Banks</option>
+                        <option value="mtr">MTR</option>
+                    </select>
+                    <button type="submit" name="id" value=<?php echo $id?>>Submit</button><br> <br>
 
               </form>
 
